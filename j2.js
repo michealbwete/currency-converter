fb985c78e6f228dd91b568833f181b6701c48ee7
michealbwete <michealbwete3@gmail.com>
2025-03-17 17:31:55 +0300

    Create j2.js

---
+++ j2.js
@@ -0,0 +1,16 @@
+getButton.addEventListener("click", e=>{
+    e.preventDefault();
+    getExchangeRate();
+} )
+
+function getExchangeRate(){
+    const amount = document.querySelector("amoount input")
+    let amountVal = amountValue;
+}
+
+
+if(amountVal == amountVal == "0"){
+    amountVal.value = "1";
+    amountVal = "1";
+
+}  
